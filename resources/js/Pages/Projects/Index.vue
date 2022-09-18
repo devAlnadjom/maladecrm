8a3a38d0790e3e906ef0f3a4fe745926b8e53cb5
alnadjom <dev.aldjom@gmail.com>
2022-09-18 01:22:57 -0400

    v.0.67

--- resources/js/Pages/Projects/Index.vue
+++ resources/js/Pages/Projects/Index.vue
@@ -87,69 +87,50 @@ defineProps({
                     </table>
 
 
-
-
                 </div>
 
-                <div class="mt-6 grid md:grid-cols-3 lg:grid-cols-4 gap-4">
-                        <div class="p-6 max-w-sm bg-gray-700 rounded-lg border hover:bg-gray-900 transition-all duration-700 hover:scale-[1.05] border-gray-800 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <div class="mt-6 grid md:grid-cols-3 lg:grid-cols-3 gap-4">
+                        <div class="p-6 max-w-sm bg-white rounded-lg border-b-4 hover:bg-white transition-all duration-700 hover:scale-[1.03] border-gray-400 shadow-md dark:bg-gray-800 dark:border-gray-700">
 
-                            <a href="#">
-                                <h5 class="mb-2 text-xl font-semibold tracking-tight text-gray-100  dark:text-white">Need a help in Claim?</h5>
-                            </a>
-                            <div class="flex justify-between text-white pb-1 border-b borrder-white">
+                            <div class=" flex justify-between">
+                                <span class="p-1 rounded bg-red-200 text-red-600 text-sm">Color</span>
+                                <span class="p-1 rounded bg-gray-200 text-gray-600 text-sm">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                                    </svg>
+
+                                </span>
+                            </div>
+                            <h3 class="my-4 font-bold text-lg"> Project Title</h3>
+                            <div class="flex .justify-between text-gray-600 pb-2 ">
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-6 h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-4 h-4">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                                     </svg>
                                 </span>
-                                <span class="text-sm">2022-02-20</span>
+                                <span class="text-sm mx-2"> 22 Jun, 2022 </span>
+
+                                <span class="ml-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-4 h-4">
+                                   <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+                                </svg>
+
+                                </span>
+                                <span class="ml-2 text-sm">10 Tasks</span>
                             </div>
-                            <div class="flex justify-between text-white pt-2 pb-1 border-b borrder-white">
-                                <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-5 h-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+                            <p class="mb-3 font-normal text-sm text-gray-500  dark:text-gray-400">Web Development</p>
+                            <div class="flex  text-gray-600">
+                                <span class="p-1 bg-gray-200 rounded-full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                                     </svg>
 
                                 </span>
-                                <span class="text-sm">10 Tasks</span>
+                                <span class="text-sm mx-2 p-1"> Client Name</span>
                             </div>
-                            <p class="mb-3 font-normal text-sm text-white dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
-                            <a href="#" class="inline-flex items-center text-blue-100 hover:underline">
-                                See our guideline
-                                <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
-                            </a>
                         </div>
-                        <div class="p-6 max-w-sm bg-blue-700 rounded-lg border border-blue-800 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                            <a href="#">
-                                <h5 class="mb-2 text-xl font-semibold tracking-tight text-gray-100  dark:text-white">Need a help in Claim?</h5>
-                            </a>
-                            <p class="mb-3 font-normal text-white dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
-                            <a href="#" class="inline-flex items-center text-blue-100 hover:underline">
-                                See our guideline
-                                <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
-                            </a>
-                        </div>
-                        <div class="p-6 max-w-sm bg-violet-700 rounded-lg border border-violet-800 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                            <a href="#">
-                                <h5 class="mb-2 text-xl font-semibold tracking-tight text-gray-100  dark:text-white">Need a help in Claim?</h5>
-                            </a>
-                            <p class="mb-3 font-normal text-white dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
-                            <a href="#" class="inline-flex items-center text-blue-100 hover:underline">
-                                See our guideline
-                                <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
-                            </a>
-                        </div>
-                        <div class="p-6 max-w-sm bg-green-700 rounded-lg border border-green-800 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                            <a href="#">
-                                <h5 class="mb-2 text-xl font-semibold tracking-tight text-gray-100  dark:text-white">Need a help in Claim?</h5>
-                            </a>
-                            <p class="mb-3 font-normal text-white dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
-                            <a href="#" class="inline-flex items-center text-blue-100 hover:underline">
-                                See our guideline
-                                <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
-                            </a>
-                        </div>
+
+
                 </div>
 
                 <div class="mt-3 flex flex-row-reverse ">
